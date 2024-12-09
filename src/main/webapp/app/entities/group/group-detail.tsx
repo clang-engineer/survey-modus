11bf67671a553621e1e184572c97a7e6554a0426
clang-engineer <clang.engineer@gmail.com>
2024-12-09 13:34:15 +0900

    feat: group detail layout

--- src/main/webapp/app/entities/group/group-detail.tsx
+++ src/main/webapp/app/entities/group/group-detail.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { Button, Col, Row } from 'reactstrap';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Translate } from 'react-jhipster';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useAppDispatch, useAppSelector } from 'app/config/store';
 
 import { getEntity } from './group.reducer';
+import MainCard from 'app/berry/ui-component/cards/MainCard';
+import { Button, ButtonGroup, Grid, Stack, Typography } from '@mui/material';
+import { gridSpacing } from 'app/berry/store/constant';
+import { IconArrowBack, IconPencil } from '@tabler/icons';
 
 export const GroupDetail = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   const { id } = useParams<'id'>();
 
@@ -18,56 +21,52 @@ export const GroupDetail = () => {
 
   const groupEntity = useAppSelector(state => state.group.entity);
   return (
-    <Row>
-      <Col md="8">
-        <h2 data-cy="groupDetailsHeading">
-          <Translate contentKey="exformmakerApp.group.detail.title">Group</Translate>
-        </h2>
-        <dl className="jh-entity-details">
-          <dt>
-            <span id="id">
-              <Translate contentKey="global.field.id">ID</Translate>
-            </span>
-          </dt>
-          <dd>{groupEntity.id}</dd>
-          <dt>
-            <span id="title">
-              <Translate contentKey="exformmakerApp.group.title">Title</Translate>
-            </span>
-          </dt>
-          <dd>{groupEntity.title}</dd>
-          <dt>
-            <span id="description">
-              <Translate contentKey="exformmakerApp.group.description">Description</Translate>
-            </span>
-          </dt>
-          <dd>{groupEntity.description}</dd>
-          <dt>
-            <span id="activated">
-              <Translate contentKey="exformmakerApp.group.activated">Activated</Translate>
-            </span>
-          </dt>
-          <dd>{groupEntity.activated ? 'true' : 'false'}</dd>
-          <dt>
+    <MainCard>
+      <Grid container spacing={gridSpacing}>
+        <Grid item xs={12}>
+          <Stack direction="row" spacing={2}>
+            <Translate contentKey="global.field.id">ID</Translate>
+            <Typography>{groupEntity.id}</Typography>
+          </Stack>
+        </Grid>
+        <Grid item xs={12}>
+          <Stack direction="row" spacing={2}>
+            <Translate contentKey="exformmakerApp.group.title">Title</Translate>
+            <Typography> {groupEntity.title} </Typography>
+          </Stack>
+        </Grid>
+        <Grid item xs={12}>
+          <Stack direction="row" spacing={2}>
+            <Translate contentKey="exformmakerApp.group.description">Description</Translate>
+            <Typography> {groupEntity.description} </Typography>
+          </Stack>
+        </Grid>
+        <Grid item xs={12}>
+          <Stack direction="row" spacing={2}>
+            <Translate contentKey="exformmakerApp.group.activated">Activated</Translate>
+            <Typography> {groupEntity.activated ? 'true' : 'false'} </Typography>
+          </Stack>
+        </Grid>
+        <Grid item xs={12}>
+          <Stack direction="row" spacing={2}>
             <Translate contentKey="exformmakerApp.group.user">User</Translate>
-          </dt>
-          <dd>{groupEntity.user ? groupEntity.user.login : ''}</dd>
-        </dl>
-        <Button tag={Link} to="/group" replace color="info" data-cy="entityDetailsBackButton">
-          <FontAwesomeIcon icon="arrow-left" />{' '}
-          <span className="d-none d-md-inline">
-            <Translate contentKey="entity.action.back">Back</Translate>
-          </span>
-        </Button>
-        &nbsp;
-        <Button tag={Link} to={`/group/${groupEntity.id}/edit`} replace color="primary">
-          <FontAwesomeIcon icon="pencil-alt" />{' '}
-          <span className="d-none d-md-inline">
-            <Translate contentKey="entity.action.edit">Edit</Translate>
-          </span>
-        </Button>
-      </Col>
-    </Row>
+            <Typography> {groupEntity.user ? groupEntity.user.login : ''} </Typography>
+          </Stack>
+        </Grid>
+        <Grid item xs={12}>
+          <ButtonGroup variant="contained" size="small">
+            <Button onClick={() => navigate('/group')} data-cy="entityDetailsBackButton">
+              <IconArrowBack size={'1rem'} />
+              <Translate contentKey="entity.action.back">Back</Translate>
+            </Button>
+            <Button onClick={() => navigate(`/group/${groupEntity.id}/edit`)} color="secondary">
+              <IconPencil size={'1rem'} />
+              <Translate contentKey="entity.action.edit">Edit</Translate>
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    </MainCard>
   );
 };
 
