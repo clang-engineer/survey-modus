e0fa9de0b283fcebd49d094836d12516ca74527e
clang-engineer <clang.engineer@gmail.com>
2024-12-24 13:06:26 +0900

    chore: const

---
+++ src/main/kotlin/com/clangengineer/exformmaker/domain/enumeration/FieldType.kt
@@ -0,0 +1,15 @@
+package com.clangengineer.exformmaker.domain.enumeration
+
+enum class FieldType {
+  TEXT,
+  TEXTAREA,
+  RADIO,
+  SELECT_BOX,
+  CHECK_BOX,
+  DATE,
+  DATETIME,
+  TIME,
+  BOOLEAN,
+  INTEGER,
+  FLOAT,
+}
