1c58e26544a76324a2105966e9815c8948d9ee05
clang-engineer <clang.engineer@gmail.com>
2024-12-27 10:30:57 +0900

    refactor: field-wizard-list-left - extract item component

---
+++ src/main/webapp/app/modules/wizard/field-wizard/field-wizard-list/component/field-wizard-list-left-item.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { getItemStyle } from 'app/modules/wizard/field-wizard/field-wizard-list/field-wizard-dnd.utils';
+import FieldWizardListLeftTitle from 'app/modules/wizard/field-wizard/field-wizard-list/component/field-wizard-list-left-title';
+import FieldWizardUpdateModal from 'app/modules/wizard/field-wizard/field-wizard-list/component/field-wizard-update.modal';
+
+import { Box, ButtonGroup, Grid, IconButton } from '@mui/material';
+import { Draggable } from 'react-beautiful-dnd';
+import { create } from 'react-modal-promise';
+import { IconPencil, IconTrash } from '@tabler/icons';
+import { IField } from 'app/shared/model/field.model';
+import useFieldWizardConfig from 'app/modules/wizard/field-wizard/field-wizard.config';
+import PromiseModal from 'app/shared/component/promise-modal';
+
+const deleteModal = create(
+  PromiseModal({
+    title: 'Delete Field',
+    content: 'Do you want to delete this field?',
+    rejectButtonText: 'Cancel',
+    resolveButtonText: 'Delete',
+  })
+);
+
+const FieldWizardListLeftItem = (props: { item: IField; index: number }) => {
+  const { item, index } = props;
+
+  const { items, setItems } = useFieldWizardConfig();
+
+  const handleDelete = id => {
+    setItems(items.filter(item => item.id !== id));
+  };
+
+  return (
+    <Draggable key={item.id} draggableId={`draggable-left-${item.id}`} index={index}>
+      {(provided, snapshot) => (
+        <Grid
+          item
+          xs={12}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
+        >
+          <Box display="flex" justifyContent="space-between" alignItems="center">
+            <FieldWizardListLeftTitle item={item} index={index} />
+            <ButtonGroup size="small">
+              <IconButton
+                onClick={() => {
+                  create(
+                    FieldWizardUpdateModal({
+                      field: item,
+                      items,
+                      setItems,
+                    })
+                  )();
+                }}
+              >
+                <IconPencil size={'1rem'} />
+              </IconButton>
+              <IconButton
+                onClick={() => {
+                  deleteModal().then(result => {
+                    if (result) handleDelete(item.id);
+                  });
+                }}
+              >
+                <IconTrash size={'0.9rem'} />
+              </IconButton>
+            </ButtonGroup>
+          </Box>
+        </Grid>
+      )}
+    </Draggable>
+  );
+};
+
+export default FieldWizardListLeftItem;
--- src/main/webapp/app/modules/wizard/field-wizard/field-wizard-list/field-wizard-list-left.tsx
+++ src/main/webapp/app/modules/wizard/field-wizard/field-wizard-list/field-wizard-list-left.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Grid } from '@mui/material';
 
-import { Box, ButtonGroup, Grid, IconButton, List, ListItem, ListItemText, Typography } from '@mui/material';
-
-import { Draggable, Droppable } from 'react-beautiful-dnd';
-import { getItemStyle, getListStyle } from 'app/modules/wizard/field-wizard/field-wizard-list/field-wizard-dnd.utils';
-
-import { IconCircle, IconPencil, IconTrash } from '@tabler/icons';
-
-import { create } from 'react-modal-promise';
-
-import PromiseModal from 'app/shared/component/promise-modal';
-import FieldWizardUpdateModal from 'app/modules/wizard/field-wizard/field-wizard-list/component/field-wizard-update.modal';
-
-import { useTheme } from '@mui/material/styles';
+import { Droppable } from 'react-beautiful-dnd';
+import { getListStyle } from 'app/modules/wizard/field-wizard/field-wizard-list/field-wizard-dnd.utils';
 import NoContentBox from 'app/shared/component/no-content-box';
 // import IFieldItem from "app/modules/wizard/field-wizard/field-wizard-list/field-item.model";
-import { IField } from 'app/shared/model/field.model';
 import useFieldWizardConfig from 'app/modules/wizard/field-wizard/field-wizard.config';
-import AnimateButton from 'app/berry/ui-component/extended/AnimateButton';
-import FieldWizardListLeftTitle from 'app/modules/wizard/field-wizard/field-wizard-list/component/field-wizard-list-left-title';
+import FieldWizardListLeftItem from 'app/modules/wizard/field-wizard/field-wizard-list/component/field-wizard-list-left-item';
 
 const EmptyDndBox = () => {
   return (
@@ -30,23 +17,8 @@ const EmptyDndBox = () => {
   );
 };
 
-const deleteModal = create(
-  PromiseModal({
-    title: 'Delete Field',
-    content: 'Do you want to delete this field?',
-    rejectButtonText: 'Cancel',
-    resolveButtonText: 'Delete',
-  })
-);
-
 const FieldWizardListLeft = () => {
-  const theme = useTheme();
-
-  const { items, setItems } = useFieldWizardConfig();
-
-  const handleDelete = id => {
-    setItems(items.filter(item => item.id !== id));
-  };
+  const { items } = useFieldWizardConfig();
 
   return (
     <Droppable droppableId="left">
@@ -58,48 +30,7 @@ const FieldWizardListLeft = () => {
               <EmptyDndBox />{' '}
             </Grid>
           ) : (
-            items.map((item, index) => (
-              <Draggable key={item.id} draggableId={`draggable-left-${item.id}`} index={index}>
-                {(provided, snapshot) => (
-                  <Grid
-                    item
-                    xs={12}
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
-                  >
-                    <Box display="flex" justifyContent="space-between" alignItems="center">
-                      <FieldWizardListLeftTitle item={item} index={index} />
-                      <ButtonGroup size="small">
-                        <IconButton
-                          onClick={() => {
-                            create(
-                              FieldWizardUpdateModal({
-                                field: item,
-                                items,
-                                setItems,
-                              })
-                            )();
-                          }}
-                        >
-                          <IconPencil size={'1rem'} />
-                        </IconButton>
-                        <IconButton
-                          onClick={() => {
-                            deleteModal().then(result => {
-                              if (result) handleDelete(item.id);
-                            });
-                          }}
-                        >
-                          <IconTrash size={'0.9rem'} />
-                        </IconButton>
-                      </ButtonGroup>
-                    </Box>
-                  </Grid>
-                )}
-              </Draggable>
-            ))
+            items.map((item, index) => <FieldWizardListLeftItem key={item.id} item={item} index={index} />)
           )}
           {provided.placeholder}
         </Grid>
