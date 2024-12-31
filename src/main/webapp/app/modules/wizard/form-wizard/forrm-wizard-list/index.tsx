3687499f5097c4bcaa2d9d0cd98a57094d45bd5d
clang-engineer <clang.engineer@gmail.com>
2024-12-31 16:06:09 +0900

    refactor: split form grid container

--- src/main/webapp/app/modules/wizard/form-wizard/forrm-wizard-list/index.tsx
+++ src/main/webapp/app/modules/wizard/form-wizard/forrm-wizard-list/index.tsx
@@ -4,29 +4,15 @@ import { Translate } from 'react-jhipster';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useAppDispatch, useAppSelector } from 'app/config/store';
 import { getEntities } from 'app/entities/form/form.reducer';
-import { Alert, Box, Button, ButtonGroup, Grid, Typography } from '@mui/material';
+import { Alert, Box, Button, Grid } from '@mui/material';
 import { gridSpacing } from 'app/berry/store/constant';
-import { IForm } from 'app/shared/model/form.model';
-import { IconClipboardList, IconPencil, IconTrash } from '@tabler/icons';
-import { useTheme } from '@mui/material/styles';
 import CheckIcon from '@mui/icons-material/Check';
-import FormDraggableGrid from 'app/modules/wizard/form-wizard/forrm-wizard-list/form-draggable-grid';
+import FormGridContainer from 'app/modules/wizard/form-wizard/forrm-wizard-list/form-grid-container';
 
-const slotProps = {
-  tooltip: {
-    sx: {
-      color: '#514E6A',
-      backgroundColor: '#ffff',
-      padding: '10px',
-      boxShadow: '0px 4px 16px rgba(0, 0, 0, 0.08)',
-    },
-  },
-};
 const FormWizardList = () => {
   const dispatch = useAppDispatch();
 
   const navigate = useNavigate();
-  const theme = useTheme();
 
   const user = useAppSelector(state => state.authentication.account);
   const formList = useAppSelector(state => state.form.entities);
@@ -47,34 +33,6 @@ const FormWizardList = () => {
 
   const handleSyncList = () => {
     getAllEntities();
-  };
-
-  const SubCardTitle = (props: { form: IForm }) => {
-    const { form } = props;
-
-    return (
-      <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Typography variant="h4">{form.title}</Typography>
-        <ButtonGroup variant="text" size="small">
-          <Button
-            onClick={() =>
-              // memo: this is the way to pass data to the next page
-              navigate(`/wizard/field`, {
-                state: { form },
-              })
-            }
-          >
-            <IconClipboardList size={'1rem'} strokeWidth={1.5} color={theme.palette.secondary.main} />
-          </Button>
-          <Button onClick={() => navigate(`/wizard/form/${form.id}/edit`)}>
-            <IconPencil size={'1rem'} strokeWidth={1.5} color={theme.palette.primary.main} />
-          </Button>
-          <Button onClick={() => navigate(`/wizard/form/${form.id}/delete`)}>
-            <IconTrash size={'1rem'} strokeWidth={1.5} color={theme.palette.error.light} />
-          </Button>
-        </ButtonGroup>
-      </Box>
-    );
   };
 
   return (
@@ -100,15 +58,13 @@ const FormWizardList = () => {
       </Grid>
       <Grid item xs={12}>
         {formList && formList.length > 0 ? (
-          <FormDraggableGrid />
+          <FormGridContainer />
         ) : (
-          !loading && (
-            <Grid item xs={12}>
-              <Alert icon={<CheckIcon fontSize="inherit" />} severity="warning">
-                <Translate contentKey="surveyModusApp.form.home.notFound">No Groups found</Translate>
-              </Alert>
-            </Grid>
-          )
+          <Grid item xs={12}>
+            <Alert icon={<CheckIcon fontSize="inherit" />} severity="warning">
+              <Translate contentKey="surveyModusApp.form.home.notFound">No Groups found</Translate>
+            </Alert>
+          </Grid>
         )}
       </Grid>
     </Grid>
