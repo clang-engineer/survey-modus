86db66c81fab57ebb678922de8f47ea9c1002846
clang-engineer <clang.engineer@gmail.com>
2025-01-17 23:46:14 +0900

    modify: datasource-file-cell.tsx - file layout

--- src/main/webapp/app/modules/survey/form-gate/datasource-file-cell.tsx
+++ src/main/webapp/app/modules/survey/form-gate/datasource-file-cell.tsx
@@ -1,37 +1,61 @@
 import React from 'react';
 import { IFile } from 'app/shared/model/file.model';
-import AnimateButton from 'app/berry/ui-component/extended/AnimateButton';
 import { downloadFileFromServer } from 'app/modules/survey/modal/component/survey-modal-file-field/file-uploader-utils';
 
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Divider, IconButton, Tooltip, Typography } from '@mui/material';
 
-import { IconFile } from '@tabler/icons';
+import { IconFileDownload } from '@tabler/icons';
+
+import { useTheme } from '@mui/material/styles';
 
 interface IDataSourceFileCellProps {
   files: IFile[];
 }
 
 const DataSourceFileCell = (props: IDataSourceFileCellProps) => {
+  const theme = useTheme();
   const { files } = props;
 
+  const TooltipTitle = (props: { file: IFile }) => {
+    const { file } = props;
+
+    return (
+      <Box
+        sx={{
+          '& .MuiTypography-root': {
+            display: 'block',
+            whiteSpace: 'nowrap',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            color: 'inherit',
+          },
+        }}
+      >
+        <Typography variant="caption">{file.name}</Typography>
+        <Divider sx={{ my: 0.5 }} />
+        <Typography variant="caption">{file['comment'] ? file['comment'] : 'No comment'}</Typography>
+      </Box>
+    );
+  };
+
   return (
-    <Box display="flex" flexDirection="row" justifyContent="center">
+    <Box>
       {files.map(file => (
-        <AnimateButton key={file.id}>
-          <Button
-            variant="text"
-            color="primary"
+        <Tooltip key={file.id} title={<TooltipTitle file={file} />} placement="top" arrow>
+          <IconButton
+            sx={{
+              color: theme.palette.text.primary,
+              '&:hover': {
+                color: theme.palette.primary.dark,
+              },
+            }}
             onClick={() => {
               downloadFileFromServer(file);
             }}
-            startIcon={<IconFile size={10} />}
           >
-            <Typography variant="caption" color="primary">
-              {' '}
-              {file.name}{' '}
-            </Typography>
-          </Button>
-        </AnimateButton>
+            <IconFileDownload size={'1rem'} />
+          </IconButton>
+        </Tooltip>
       ))}
     </Box>
   );
