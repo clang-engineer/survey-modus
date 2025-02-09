e54e362024b9b9bf12d455910786d4a7f40d8a31
clang-engineer <clang.engineer@gmail.com>
2025-02-10 08:27:12 +0900

    rename: survey router -component naming

--- src/main/webapp/app/modules/survey/index.tsx
+++ src/main/webapp/app/modules/survey/index.tsx
@@ -3,7 +3,7 @@ import { Route } from 'react-router-dom';
 
 import ErrorBoundaryRoutes from 'app/shared/error/error-boundary-routes';
 
-import SurveyRouter from './survey-router';
+import SurveyEntrance from './survey-entrance';
 import FormGate from 'app/modules/survey/gate';
 import MinimalLayout from 'app/berry/layout/MinimalLayout';
 import MainLayout from 'app/berry/layout/MainLayout';
@@ -12,7 +12,7 @@ import JhLayout from 'app/shared/layout/jh-layout';
 const GateRoutes = () => (
   <ErrorBoundaryRoutes>
     <Route element={<MinimalLayout />}>
-      <Route index element={<SurveyRouter />} />
+      <Route index element={<SurveyEntrance />} />
     </Route>
     {/*<Route element={<MainLayout />}>*/}
     <Route element={<JhLayout />}>
