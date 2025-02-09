d57df68dc5994fa0920ef5e312361230c82a3c0a
clang-engineer <clang.engineer@gmail.com>
2025-02-09 20:25:23 +0900

    fix: StaffService - parameter err

--- src/main/kotlin/com/clangengineer/surveymodus/service/StaffService.kt
+++ src/main/kotlin/com/clangengineer/surveymodus/service/StaffService.kt
@@ -1,24 +1,54 @@
 package com.clangengineer.surveymodus.service
 
+import com.clangengineer.surveymodus.domain.embeddable.Staff
+import com.clangengineer.surveymodus.security.getCurrentUserLogin
 import org.slf4j.LoggerFactory
-import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.core.RowMapper
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional
 class StaffService(
-    private val jdbcTemplate: JdbcTemplate
+    private val jdbcTemplate: NamedParameterJdbcTemplate
 ) {
     private val log = LoggerFactory.getLogger(javaClass)
 
     fun checkActivatedStaffExist(phone: String): Boolean {
         log.debug("Request to check Staff exist : $phone")
-        val sql = "SELECT COUNT(*) FROM tbl_staff WHERE activated = true AND phone = ?"
+        val sql = "SELECT COUNT(*) FROM tbl_staff WHERE activated = true AND phone = :phone"
 
-        val count = jdbcTemplate.queryForObject(sql, arrayOf(phone), RowMapper { rs, _ -> rs.getInt(1) })
+        val params = mapOf("phone" to phone)
+
+        val count = jdbcTemplate.queryForObject(sql, params, Int::class.java)
 
         return count != null && count > 0
+    }
+
+    fun getStaffSession(): Staff {
+        log.debug("REST request to get staff info")
+
+        val phone = getCurrentUserLogin().orElseThrow { RuntimeException("User could not be found") }
+
+        val sql = """
+        SELECT first_name, last_name, email, activated, lang_key, phone
+        FROM tbl_staff
+        WHERE phone = :phone
+        """.trimIndent()
+
+        val params = mapOf("phone" to phone)
+
+        return jdbcTemplate.query( sql, params
+        ) { rs, _ ->
+            Staff(
+                firstName = rs.getString("first_name"),
+                lastName = rs.getString("last_name"),
+                email = rs.getString("email"),
+                activated = rs.getBoolean("activated"),
+                langKey = rs.getString("lang_key"),
+                phone = rs.getString("phone")
+            )
+        }.firstOrNull() ?: throw RuntimeException("No staff found for phone: $phone")
     }
 }
---
+++ src/main/kotlin/com/clangengineer/surveymodus/web/rest/StaffController.kt
@@ -0,0 +1,24 @@
+package com.clangengineer.surveymodus.web.rest
+
+import com.clangengineer.surveymodus.domain.embeddable.Staff
+import com.clangengineer.surveymodus.service.StaffService
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class StaffController(private val staffService: StaffService) {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    @GetMapping("/staff-info")
+    fun getStaffInfo(): ResponseEntity<Staff> {
+        log.debug("REST request to get staff info")
+
+        val staff = staffService.getStaffSession()
+
+        return ResponseEntity.ok(staff)
+    }
+}
